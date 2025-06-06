80f709db5e7f8d8e4f49a7a1e4742b3e19a10346
Angela Jomy <angelajomy87@gmail.com>
2025-06-06 20:27:41 +0530

    add navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import Sponsors from './Sponsors';
-import './App.css'
-import Hero from'./Pages/Hero/Hero';
-import About from './Pages/About/About'
-import FAQ from './Pages/FAQ/faq.jsx'
-import Footer from './Pages/Footer/Footer.jsx';
-import Timeline from './Pages/Timeline/Timeline.jsx'
-function App() {
+import './App.css';
+import Navbar from './Pages/Navbar/Navbar.jsx';
 
+import Hero from './Pages/Hero/Hero';
+import About from './Pages/About/About';
+import Timeline from './Pages/Timeline/Timeline.jsx';
+import FAQ from './Pages/FAQ/faq.jsx';
+import Footer from './Pages/Footer/Footer.jsx';
+import Sponsors from './Sponsors';
+
+function App() {
   return (
-    <>
-      <Hero />
-      <About/>
-      <Timeline/>
-      <FAQ/>
+    <div className="bg-black text-white scroll-smooth">
+      {/* NAVBAR ONLY INSIDE HERO */}
+      <section id="home">
+        <Navbar />
+        <Hero />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="timeline">
+        <Timeline />
+      </section>
+
+      <section id="sessions">
+        <FAQ />
+      </section>
+
+      <section id="sponsers">
+        <Sponsors />
+      </section>
+
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
