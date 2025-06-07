5c834cb2a69a6aa9cac04206f129230f6d783ef5
Fidha Ahamed <fidhaahamedpp@gmail.com>
2025-06-07 13:07:06 +0530

    app.jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
-
-import './App.css';
-import Navbar from './Pages/Navbar/Navbar.jsx';
-
-import Hero from './Pages/Hero/Hero';
-import About from './Pages/About/About';
-import Timeline from './Pages/Timeline/Timeline';
+import './App.css'
+import Navbar from './Pages/Navbar/Navbar.jsx'
+import Hero from'./Pages/Hero/Hero';
+import About from './Pages/About/About'
 import Tracks from './Pages/Tracks/Tracks';
-import FAQ from './Pages/FAQ/faq.jsx';
-import Sponsors from './Sponsors';
+import Timeline from './Pages/Timeline/Timeline.jsx' 
+import Sponsors from './Pages/Sponsors/Sponsors.jsx';
+import FAQ from './Pages/FAQ/faq.jsx'
 import Footer from './Pages/Footer/Footer.jsx';
 
 function App() {
+
   return (
-    <div>
-      <section id="home" className="relative min-h-screen">
-        <Navbar />
-        <Hero />
-      </section>
-
-      <section id="about">
-        <About />
-      </section>
-
-      <section id="timeline">
-        <Timeline />
-      </section>
-
-      <section id="tracks">
-        <Tracks />
-      </section>
-
-      <section id="faq">
-        <FAQ />
-      </section>
-
-      <section id="sponsors">
-        <Sponsors />
-      </section>
+    <>
+      <Navbar />
+      <Hero />      
+      <About/>
+      <Tracks />
+      <Timeline />
+      <Sponsors/>
+      <FAQ/>
       <Footer />
     </>
   )
